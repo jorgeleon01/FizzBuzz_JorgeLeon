6a1e7afa9a0ac1a294a621062fe895e2c2c4b5e3
JorgeLeon <jorgeleonvivas@gmail.com>
2019-03-01 00:07:00 -0600

    5-Agrego unos comentarios y un mensaje de inicio

--- Solution.py
+++ Solution.py
@@ -1,6 +1,9 @@
+print("Solucion en rango del 1 al 20")
 for j in range(1,21):
+    #Condicion para multiplos de 3
     if j%3==0:
         print("Fizz")
+    #Condicion para multiplos de 5
     elif j%5==0:
         print("Buzz")
     else:
