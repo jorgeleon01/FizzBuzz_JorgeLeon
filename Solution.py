7c1bf3ef9fa04bdbe87781b9de0090c481df2e56
JorgeLeon <jorgeleonvivas@gmail.com>
2019-03-01 00:12:04 -0600

    6-Declaro la condicion para multiplos de 3 y 5

--- Solution.py
+++ Solution.py
@@ -1,7 +1,10 @@
 print("Solucion en rango del 1 al 20")
 for j in range(1,21):
+    #condicion para multiplos de 3 y 5
+    if j%3==0 and j%5==0:
+        print("FizzBuzz")
     #Condicion para multiplos de 3
-    if j%3==0:
+    elif j%3==0:
         print("Fizz")
     #Condicion para multiplos de 5
     elif j%5==0:
